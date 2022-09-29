9215fcad6535a2c496320d8861d3b06e2764fae7
Dalton Hulett <dhulett3@huskers.unl.edu>
2022-09-29 15:45:35 -0500

    Document storage prototype
    Still WIP for how files will persist across runs, also not currently saving the filepath to the db

--- StarterProject/HelloFlask/__init__.py
+++ StarterProject/HelloFlask/__init__.py
@@ -1,4 +1,11 @@
 from flask import Flask
+from os.path import join, dirname, realpath
+
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecretkey' # TODO: Secret key stuffs (required for flash)
+# Trick from stackoverflow to generate the absolute path since the relative path was not working as demonstrated in the flask tutorial
+app.config['UPLOAD_FOLDER'] = join(dirname(realpath(__file__)), 'static/uploads')
+app.config['ALLOWED_EXTENSIONS'] = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 import HelloFlask.views
--- StarterProject/HelloFlask/views.py
+++ StarterProject/HelloFlask/views.py
@@ -1,35 +1,82 @@
 from datetime import datetime
-from flask import Flask
-from flask import render_template
-from flask import request
+from flask import (
+    Flask,
+    flash,
+    redirect,
+    render_template,
+    request,
+    send_from_directory,
+    url_for,
+)
 from HelloFlask import app
+import os
+from werkzeug.utils import secure_filename
 
 
-@app.route('/')
-@app.route('/home')
+def allowed_file(filename):
+    return (
+        "." in filename
+        and filename.rsplit(".", 1)[1].lower() in app.config["ALLOWED_EXTENSIONS"]
+    )
+
+
+@app.route("/uploads/<path:name>")
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name, as_attachment=True)
+
+
+@app.route("/upload", methods=["GET", "POST"])
+def upload():
+    if request.method == "POST":
+        # check if the post request has the file part
+        if "file" not in request.files:
+            flash("No file part")
+            return redirect(request.url)
+        upload = request.files["file"]
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if upload.filename == "":
+            flash("No selected file")
+            return redirect(request.url)
+        if upload and allowed_file(upload.filename):
+            filename = secure_filename(upload.filename)
+            upload.save(
+                os.path.join(app.config["UPLOAD_FOLDER"], filename)
+            )  # TODO: Add timestamp or something to prevent duplicate names overwriting each other?
+            return redirect(url_for("download_file", name=filename))
+    return render_template("upload.html", title="Document Storage Prototype")
+
+
+@app.route("/")
+@app.route("/home")
 def home():
     now = datetime.now()
     formatted_now = now.strftime("%A, %d %B, %Y at %X")
 
     return render_template(
         "index.html",
-        title = "Hello Flask",
-        message = "Hello, Flask!",
-        content = " on " + formatted_now)
+        title="Hello Flask",
+        message="Hello, Flask!",
+        content=" on " + formatted_now,
+    )
 
-@app.route('/hello')
-@app.route('/hello/<name>')
-def hello(name = None):
+
+@app.route("/hello")
+@app.route("/hello/<name>")
+def hello(name=None):
     msg = request.args.get("message", None)
-    return f"Hello {'World' if name is None else name}!{'' if msg is None else ' ' + msg}"
+    return (
+        f"Hello {'World' if name is None else name}!{'' if msg is None else ' ' + msg}"
+    )
 
-@app.route('/api/data')
+
+@app.route("/api/data")
 def get_data():
-  return app.send_static_file('data.json')
+    return app.send_static_file("data.json")
 
-@app.route('/about')
+
+@app.route("/about")
 def about():
     return render_template(
-        "about.html",
-        title = "About HelloFlask",
-        content = "Example app page for Flask.")
+        "about.html", title="About HelloFlask", content="Example app page for Flask."
+    )
